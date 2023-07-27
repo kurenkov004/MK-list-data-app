33ad9021d9221ea0f6d31ae9f35ca0e230d524e6
kurenkov004 <mary.kurenkov@gmail.com>
2023-07-26 20:10:16 -0400

    created initial pokemonList object; contains three Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-alert('Hello world');
+// Charmander is #4, Totodile is #158, Persian is #53 - for future reference, because I'm clueless about Pokemon.
 
-let favoriteFood = 'Sushi';
-
-document.write(favoriteFood);
-
+pokemonList=[
+  {name: 'Charmander', type: ['fire', 'lizard'], height: 0.6 },
+  {name: 'Totodile', type: ['water', 'biting'], height: 0.6 },
+  {name: 'Persian', type: ['cat', 'limber'], height: 1}
+]
 
