c515c42d8529ba93dfdd76ed2da68eb3b6b0838d
kurenkov004 <mary.kurenkov@gmail.com>
2023-08-01 15:27:17 -0400

    created "for" loop to list pokemon names & heights in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 // Charmander is #4, Totodile is #158, Persian is #53 - for future reference, because I'm clueless about Pokemon.
 
-pokemonList=[
+let pokemonList=[
   {name: 'Charmander', type: ['fire', 'lizard'], height: 0.6 },
   {name: 'Totodile', type: ['water', 'biting'], height: 0.6 },
   {name: 'Persian', type: ['cat', 'limber'], height: 1}
 ]
 
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if(i == 1) {
+    document.write('<p>' + pokemonList[i].name + ' height: ' + pokemonList[i].height + '  - Wow, that\'s dangerous! </p>');
+  }else{
+    document.write('<p>' + pokemonList[i].name + ' height: ' + pokemonList[i].height);
+  }
+}
