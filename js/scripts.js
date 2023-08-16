ff59afd32fbda17b21be66dc51c67f668d47a9c6
kurenkov004 <mary.kurenkov@gmail.com>
2023-08-16 12:05:38 -0400

    switched to using forEach method to print Pokemon details

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 // Charmander is #4, Totodile is #158, Persian is #53 - for future reference, because I'm clueless about Pokemon.
 
-let pokemonList=[
+let pokemonList = [
   {name: 'Charmander', type: ['fire', 'lizard'], height: 0.6 },
   {name: 'Totodile', type: ['water', 'biting'], height: 0.6 },
   {name: 'Persian', type: ['cat', 'limber'], height: 1}
-]
+];
+
+pokemonList.forEach(listNames);
+  function listNames(item) {
+    document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
+  }
 
 
-for (let i = 0; i < pokemonList.length; i++) {
+
+
+
+
+
+/*for (let i = 0; i < pokemonList.length; i++) {
   if(i == 1) {
     document.write('<p id="list_items">' + pokemonList[i].name + ' (height) ' + pokemonList[i].height + '  - Wow, that\'s dangerous! </p>');
   }else{
     document.write('<p id="list_items">' + pokemonList[i].name + ' (height) ' + pokemonList[i].height);
   }
-}
+} 
+*/
+
