a716f7739a1b497c1254beb2ce38262e5739b3e2
kurenkov004 <mary.kurenkov@gmail.com>
2023-08-16 12:40:12 -0400

    wrapped pokemonList in IIFE & updated forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
 // Charmander is #4, Totodile is #158, Persian is #53 - for future reference, because I'm clueless about Pokemon.
 
-let pokemonList = [
-  {name: 'Charmander', type: ['fire', 'lizard'], height: 0.6 },
-  {name: 'Totodile', type: ['water', 'biting'], height: 0.6 },
-  {name: 'Persian', type: ['cat', 'limber'], height: 1}
-];
-
-pokemonList.forEach(listNames);
-  function listNames(item) {
-    document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
-  }
-
-
-
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {name: 'Charmander', type: ['fire', 'lizard'], height: 0.6 },
+    {name: 'Totodile', type: ['water', 'biting'], height: 0.6 },
+    {name: 'Persian', type: ['cat', 'limber'], height: 1}
+  ];
+  
+  //I initially left the forEach loop inside the IIFE and it worked the same. just leaving this here for later reference, I'll probably delete it soon.
+  // pokemonList.forEach(listNames);
+  //   function listNames(item) {
+  //     document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
+  //   }
+  
+    function getAll () {
+      return pokemonList;
+    }
+    function add (item) {
+      pokemonList.push(item);
+    }
+    return {
+    getAll: getAll,
+    add: add
+    }
+  
+  })();
+  
+  
+pokemonRepository.getAll().forEach(listNames);
+      function listNames(item) {
+      document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
+    }
 
 
 
