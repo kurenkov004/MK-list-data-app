7712f0ec36dd62a782014f1bcf1b8a71f977042b
kurenkov004 <mary.kurenkov@gmail.com>
2023-08-21 18:16:03 -0400

    created button and attached an event listener to it

--- js/scripts.js
+++ js/scripts.js
@@ -6,31 +6,46 @@ let pokemonRepository = (function(){
     {name: 'Totodile', type: ['water', 'biting'], height: 0.6 },
     {name: 'Persian', type: ['cat', 'limber'], height: 1}
   ];
-  
-  //I initially left the forEach loop inside the IIFE and it worked the same. just leaving this here for later reference, I'll probably delete it soon.
-  // pokemonList.forEach(listNames);
-  //   function listNames(item) {
-  //     document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
-  //   }
-  
+   
     function getAll () {
       return pokemonList;
     }
     function add (item) {
       pokemonList.push(item);
     }
+
+    function addListItem (pokemon) {
+      let listOfPokemon = document.querySelector('.pokemon-list');
+      let listItem = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('pokemon-button');
+      listItem.appendChild(button);
+      listOfPokemon.appendChild(listItem);
+      button.addEventListener('click', function () {
+        showDetails(pokemon.name);
+      });
+    }
+    function showDetails (pokemon) {
+      console.log(pokemon);
+    }
     return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem,
     }
   
   })();
   
   
-pokemonRepository.getAll().forEach(listNames);
-      function listNames(item) {
-      document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
-    }
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon)
+
+});
+      // function listNames(_pokemon) {
+      //   addListItem(item);
+      // document.write('<p id="list_items">' + item.name + ' (height) ' + item.height + '</p>');
+
 
 
 
